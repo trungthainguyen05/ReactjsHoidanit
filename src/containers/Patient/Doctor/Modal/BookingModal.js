767722afa5a4c1ab6702d841795695a67a279fe0
trungthainguyen05 <63950167+trungthainguyen05@users.noreply.github.com>
2022-11-20 19:56:43 +0700

    finish bai_82

---
+++ src/containers/Patient/Doctor/Modal/BookingModal.js
@@ -0,0 +1,123 @@
+import React, { Component } from 'react';
+import { connect } from "react-redux";
+import { FormattedMessage } from 'react-intl';
+import './BookingModal.scss';
+import { Modal } from 'reactstrap';
+
+class BookingModal extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+
+        }
+    }
+
+    async componentDidMount() {
+
+    }
+
+    componentDidUpdate(prevProps, PrevState, SnapShot) {
+        if (prevProps.language !== this.props.language) {
+
+        }
+    }
+    // toggle={ }
+    render() {
+        let { isOpenModal, closeBookingModal, dataTime } = this.props;
+        return (
+            <Modal
+                isOpen={isOpenModal}
+                className={'booking-modal-container'}
+                size="lg"
+                centered
+            >
+                <div className="booking-modal-content">
+                    <div className="booking-modal-header">
+                        <span className="left">Thông tin đặt lịch khám bệnh</span>
+                        <span
+                            className="right"
+                            onClick={closeBookingModal}
+                        >
+                            <i className="fas fa-times"></i>
+                        </span>
+                    </div>
+                    <div className="booking-modal-body">
+                        <div className="doctor-infor">
+
+                        </div>
+                        <div className="price">
+                            Giá khám
+                        </div>
+
+                        <div className="container">
+                            <div className="row">
+                                <div className="col-6">
+                                    <label>Họ tên</label>
+                                    <input className="form-control" />
+                                </div>
+                                <div className="col-6">
+                                    <label>Số điện thoại</label>
+                                    <input className="form-control" />
+                                </div>
+                                <div className="col-6">
+                                    <label>Địa chỉ Email</label>
+                                    <input className="form-control" />
+                                </div>
+                                <div className="col-6">
+                                    <label>Địa chỉ liên hệ</label>
+                                    <input className="form-control" />
+                                </div>
+                                <div className="col-12">
+                                    <label>Lý do khám</label>
+                                    <input className="form-control" />
+                                </div>
+                                <div className="col-6">
+                                    <label>Đặt cho ai</label>
+                                    <input className="form-control" />
+                                </div>
+                                <div className="col-6">
+                                    <label>Giới tính</label>
+                                    <input className="form-control" />
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>
+                    <div className="booking-modal-footer">
+                        <button
+                            className="btn-booking-confirm"
+                            onClick={closeBookingModal}
+                        >
+                            Xác nhận
+                        </button>
+                        <button
+                            className="btn-booking-cancel"
+                            onClick={closeBookingModal}
+                        >
+                            Hủy
+                        </button>
+                    </div>
+                </div>
+
+                <div style={{ height: '100px' }}>
+                    Hello from inside modal
+                </div>
+
+            </Modal>
+        );
+    }
+}
+
+const mapStateToProps = state => {
+    return {
+        language: state.app.language,
+    };
+};
+
+const mapDispatchToProps = dispatch => {
+    return {
+    };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(BookingModal);
