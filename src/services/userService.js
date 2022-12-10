fc15d1ef6959cf4e85eb51bfd84841e48fe35a70
trungthainguyen05 <63950167+trungthainguyen05@users.noreply.github.com>
2022-12-10 16:03:29 +0700

    create Manage Clinic in admin and init data

--- src/services/userService.js
+++ src/services/userService.js
@@ -81,6 +81,10 @@ const getAllDetailSpecialty = (data) => {
     return axios.get(`/api/get-detail-specialty-by-id?id=${data.id}&location=${data.location}`);
 }
 
+const createNewClinic = (data) => {
+    return axios.post('/api/create-clinic', data);
+}
+
 export {
     handleLoginApi, getAllUsers,
     createNewUserService, deleteUserService,
@@ -91,6 +95,6 @@ export {
     getExtraInforDoctorById, getProfileDoctorById,
     postPatientBookAppointment, postVerifyBookAppointment,
     createNewSpecialty, getAllSpecialty,
-    getAllDetailSpecialty,
+    getAllDetailSpecialty, createNewClinic
 
 }
